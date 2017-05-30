82f1f58814f86448dfe519195050afac636fe1d4
DavidWallJ <djkindread@gmail.com>
2017-05-30 16:36:57 +0800

    added filter functionality

--- app/tests/api/TodoAPI.test.jsx
+++ app/tests/api/TodoAPI.test.jsx
@@ -61,4 +61,51 @@ describe('TodoAPI', () => {
     })
 
   });
+
+  describe('filterTodos', () => {
+      const todos = [{
+        id: 1,
+        text: 'some text here',
+        completed: true
+      },{
+        id: 2,
+        text: 'some text again',
+        completed: true
+      },{
+        id: 3,
+        text: 'some text one more time',
+        completed: false
+      }];
+
+      it('should return all items if showComplete is true', () => {
+        const filteredTodos = TodoAPI.filterTodos(todos, true, '');
+
+        expect(filteredTodos.length).toBe(3);
+      });
+
+    it('should return only non-completed items when show completed is false', () => {
+      const filteredTodos = TodoAPI.filterTodos(todos, false, '');
+
+      expect(filteredTodos.length).toBe(1);
+    });
+
+    it('should sort by completed status', () => {
+      const filteredTodos = TodoAPI.filterTodos(todos, true, '');
+      // we would expect all 3 items and completed false to be at the beginning of the array
+      expect(filteredTodos[0].completed).toBe(false);
+    });
+
+    it('should filter todos by searchText', () => {
+      const filteredTodos = TodoAPI.filterTodos(todos, false, 'time');
+
+      expect(filteredTodos.length).toBe(1);
+    });
+
+    it('should return all todos if searchText is empty', () => {
+      const filteredTodos = TodoAPI.filterTodos(todos, true, '');
+
+      expect(filteredTodos.length).toBe(3);
+    });
+
+  });
 })
