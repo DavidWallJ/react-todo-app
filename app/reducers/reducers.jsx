b0e174828f5433037817f78d87c9d1f6202c27c0
DavidWallJ <djkindread@gmail.com>
2017-06-02 20:29:36 +0800

    add todos reducer and tests

--- app/reducers/reducers.jsx
+++ app/reducers/reducers.jsx
@@ -1,6 +1,9 @@
 /**
  * Created by david on 6/2/17.
  */
+import uuid from 'node-uuid';
+import moment from 'moment';
+
 export const searchTextReducer = (state = '', action) => {
     switch (action.type) {
       case 'SET_SEARCH_TEXT':
@@ -16,6 +19,36 @@ export const showCompletedReducer = (state = 'false', action) => {
     switch (action.type) {
       case 'TOGGLE_SHOW_COMPLETED':
         return !state;
+      default:
+        return state;
+    }
+};
+
+export const todosReducer = (state = [], action) => {
+    switch (action.type) {
+      case 'ADD_TODO':
+        return [
+          ...state,
+          {
+            text: action.text,
+            id: uuid(),
+            completed: false,
+            createdAt: moment().unix(),
+            completedAt: undefined
+          }
+        ];
+      case 'TOGGLE_TODO':
+        return state.map((todo) => {
+          if (todo.id === action.id) {
+            const nextCompleted = !todo.completed;
+
+            return {
+              ...todo,
+              completed: nextCompleted,
+              completedAt: nextCompleted ? moment().unix() : undefined
+            };
+          }
+      });
       default:
         return state;
     }
--- app/tests/reducers/reducers.test.jsx
+++ app/tests/reducers/reducers.test.jsx
@@ -1,36 +1,68 @@
 /**
  * Created by david on 6/2/17.
  */
-import expect from 'expect';
-import df from 'deep-freeze-strict';
-// we don't need to use df but i think it gives you better error messages or something
-const reducers = require('reducers');
+import expect from 'expect'
+import df from 'deep-freeze-strict'
+// we don't need to use df but i think it gives you better error messages or something or make something easier
+const reducers = require('reducers')
 
 describe('Reducers', () => {
   describe('searchTextReducer', () => {
-      it('should set searchText', () => {
-          const action = {
-            type: 'SET_SEARCH_TEXT',
-            searchText: 'sup dog'
-          };
+    it('should set searchText', () => {
+      const action = {
+        type: 'SET_SEARCH_TEXT',
+        searchText: 'sup dog'
+      }
 
-          const res = reducers.searchTextReducer(df(''), df(action));
+      const res = reducers.searchTextReducer(df(''), df(action))
 
-          expect(res).toEqual(action.searchText);
-      });
-  });
+      expect(res).toEqual(action.searchText)
+    })
+  })
 
   describe('showCompletedReducer', () => {
     it('should toggle showCompleted', () => {
       const action = {
         type: 'TOGGLE_SHOW_COMPLETED',
+      }
+
+      const res = reducers.showCompletedReducer(df(false), df(action))
+
+      expect(res).toEqual(true)
+    })
+  })
+
+  describe('todosReducer', () => {
+    it('should add new todo', () => {
+      const action = {
+        type: 'ADD_TODO',
+        text: 'Water the cat'
+      }
+
+      const res = reducers.todosReducer(df([]), df(action))
+
+      expect(res.length).toEqual(1)
+      expect(res[0].text).toEqual(action.text)
+    })
+
+    it('should toggle todo', () => {
+      const todos = [{
+        id: '123',
+        text: 'something',
+        completed: true,
+        createdAt: 123,
+        completedAt: 126
+      }];
+
+      const action = {
+        type: 'TOGGLE_TODO',
+        id: '123'
       };
 
-      const res = reducers.showCompletedReducer(df(false), df(action));
+      const res = reducers.todosReducer(df(todos), df(action));
 
-      expect(res).toEqual(true);
+      expect(res[0].completed).toEqual(false);
+      expect(res[0].completedAt).toEqual(undefined);
     });
-  });
-
-
-});
+  })
+})
