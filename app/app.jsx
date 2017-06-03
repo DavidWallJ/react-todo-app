e9dd0780638d525e11ca57e73ffbca22ee302483
DavidWallJ <djkindread@gmail.com>
2017-06-03 17:41:28 +0800

    added redux store

--- app/app.jsx
+++ app/app.jsx
@@ -4,6 +4,16 @@ import {Route, Router, IndexRoute, hashHistory} from 'react-router';
 
 import TodoApp from 'TodoApp';
 
+const actions = require('actions');
+const store = require('configureStore').configure();
+
+store.subscribe(() => {
+    console.log('New state: ', store.getState());
+});
+
+store.dispatch(actions.addTodo('Water the cat'));
+store.dispatch(actions.setSearchText('yard'));
+store.dispatch(actions.toggleShowCompleted());
 // Load foundation
 $(document).foundation();
 
--- app/reducers/reducers.jsx
+++ app/reducers/reducers.jsx
@@ -15,7 +15,7 @@ export const searchTextReducer = (state = '', action) => {
 
 // showCompletedReducer, default false, TOGGLE_SHOW_COMPLETED
 
-export const showCompletedReducer = (state = 'false', action) => {
+export const showCompletedReducer = (state = false, action) => {
     switch (action.type) {
       case 'TOGGLE_SHOW_COMPLETED':
         return !state;
